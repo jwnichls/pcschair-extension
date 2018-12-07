3db064c4248e0eb3f2f78f78ade6206ed4c85cc5
Jeffrey Nichols <jwnichols@google.com>
2018-12-06 22:47:23 -0800

    Don't Maintain State in Local Vars
    Previously, the extension maintained relevant state in local variables in its
    background page.  These pages are not persistent, even while the extension is
    in use, and thus the state may be lost frequently. It seems a recent change in
    Chrome causes this to happen much more often than previously. The solution is to
    use the Chrome storage APIs to persistently store and recover the state so it is
    not lost to background page garbage collection.

--- background.js
+++ background.js
@@ -1,25 +1,40 @@
-var TIMER_VALUE = 5;
+var STATE = {
+	TIMER_VALUE: 5,
+	EXTENSION_LINKED: false,
+	PCS_ASSIST_SERVER_HOST: "http://www.pcschair.org",
+	PCS_VENUE_ID: "1",
+	PCS_SUB_COMMITTEE_FLAG: false,
+	PCS2_FLAG: true,
+	PCS2_VENUE_NAME: "eics_pacmb",
 
-var EXTENSION_LINKED = false;
-var PCS_ASSIST_SERVER_HOST = "http://www.pcschair.org";
-var PCS_VENUE_ID = 1;
-var PCS_SUB_COMMITTEE_FLAG = false;
-var PCS2_FLAG = true;
-var PCS2_VENUE_NAME = "eics_pacmb";
+	PCS_USER_REF: null
+};
 
-var PCS_USER_REF = null;
+loadState();
+
+function loadState() {
+	chrome.storage.local.get(['extensionData'], function(result) {
+		if (result.extensionData != undefined) {
+			STATE = result.extensionData;
+		}
+	});
+}
+
+function saveState() {
+	chrome.storage.local.set({extensionData: STATE});
+}
 
 function updateVenueWithData(sendData, callback) {
-	if (EXTENSION_LINKED) {
+	if (STATE.EXTENSION_LINKED) {
 		if (sendData.timer != null) {
-			var newTime = new Date((new Date()).getTime() + TIMER_VALUE*60*1000);
+			var newTime = new Date((new Date()).getTime() + STATE.TIMER_VALUE*60*1000);
 			sendData.timer = newTime.toUTCString();
 		}
 
 		sendData = {venue: sendData};
 		$.ajax({
 		           type: "PUT",
-		           url: "http://" + PCS_ASSIST_SERVER_HOST + "/venues/" + PCS_VENUE_ID + ".json",
+		           url: "http://" + STATE.PCS_ASSIST_SERVER_HOST + "/venues/" + STATE.PCS_VENUE_ID + ".json",
 		           dataType: "json",
 		           success: callback,
 		           data: sendData
@@ -32,55 +47,60 @@ chrome.runtime.onMessage.addListener(function(message, sender, callback) {
 		updateVenueWithData(message.sendData,callback);
 	}
 	else if (message.type == "timer") {
-		TIMER_VALUE = message.timerValue;
+		STATE.TIMER_VALUE = message.timerValue;
+		saveState();
 	}
 	else if (message.type == "set-pcs-user-ref") {
-		PCS_USER_REF = message.pcsUserRef;
+		STATE.PCS_USER_REF = message.pcsUserRef;
+		saveState();
 	}
 	else if (message.type == "link-extension") {
-		PCS_ASSIST_SERVER_HOST = message.hostname;
-		PCS_VENUE_ID = message.venueID;
-		EXTENSION_LINKED = message.hostname != null && message.hostname != "" && message.venueID != null && message.venueID != "";
+		STATE.PCS_ASSIST_SERVER_HOST = message.hostname;
+		STATE.PCS_VENUE_ID = message.venueID;
+		STATE.EXTENSION_LINKED = message.hostname != null && message.hostname != "" && message.venueID != null && message.venueID != "";
 
-		if (EXTENSION_LINKED) {
+		if (STATE.EXTENSION_LINKED) {
 			$.ajax({
 			        type: "GET",
-			        url: "http://" + PCS_ASSIST_SERVER_HOST + "/venues/" + PCS_VENUE_ID + ".json",
+			        url: "http://" + STATE.PCS_ASSIST_SERVER_HOST + "/venues/" + STATE.PCS_VENUE_ID + ".json",
 			        dataType: "json",
 					error: function() { 
-						EXTENSION_LINKED = false;
-						callback({linked: EXTENSION_LINKED}); 
+						STATE.EXTENSION_LINKED = false;
+						callback({linked: STATE.EXTENSION_LINKED}); 
 					},
 					success: function(data) {
-						PCS2_FLAG = data.pcs2_flag;
-						PCS2_VENUE_NAME = data.pcs2_venue_name;
-						PCS_SUB_COMMITTEE_FLAG = data.sub_committee;
+						STATE.PCS2_FLAG = data.pcs2_flag;
+						STATE.PCS2_VENUE_NAME = data.pcs2_venue_name;
+						STATE.PCS_SUB_COMMITTEE_FLAG = data.sub_committee;
+						saveState();
 
-						callback({linked: EXTENSION_LINKED});
+						callback({linked: STATE.EXTENSION_LINKED});
 					}
 			});
 		}
 	}
 	else if (message.type == "check-link-status") {
-		callback({linked: EXTENSION_LINKED});
+		if (STATE.EXTENSION_LINKED == undefined)
+			STATE.EXTENSION_LINKED = false;
+		callback({linked: STATE.EXTENSION_LINKED});
 	}
 	else if (message.type == "open-pcs-page") {
-		if (EXTENSION_LINKED) {
-			if (PCS2_FLAG) {
+		if (STATE.EXTENSION_LINKED) {
+			if (STATE.PCS2_FLAG) {
 				chrome.tabs.query({url: 'https://new.precisionconference.com/*'}, function(results) {
 					if (results.length > 0) {
-						chrome.tabs.create({'windowId': results[0].windowId, 'index': results[0].index+1, 'url': "https://new.precisionconference.com/" + PCS2_VENUE_NAME + "/chair/subs/" + message.paperId});
+						chrome.tabs.create({'windowId': results[0].windowId, 'index': results[0].index+1, 'url': "https://new.precisionconference.com/" + STATE.PCS2_VENUE_NAME + "/chair/subs/" + message.paperId});
 					} else {
-						chrome.tabs.create({'url': "https://new.precisionconference.com/" + PCS2_VENUE_NAME + "/chair/subs/" + message.paperId});
+						chrome.tabs.create({'url': "https://new.precisionconference.com/" + STATE.PCS2_VENUE_NAME + "/chair/subs/" + message.paperId});
 					}
 				});
 			}
-			else if (PCS_USER_REF != null) {
+			else if (STATE.PCS_USER_REF != null) {
 				chrome.tabs.query({url: 'https://confs.precisionconference.com/*'}, function(results) {
 					if (results.length > 0) {
-						chrome.tabs.create({'windowId': results[0].windowId, 'index': results[0].index+1, 'url': "https://confs.precisionconference.com/~chi18a/adminOnePaper?userRef=" + PCS_USER_REF + "&paperNumber=" + message.paperId + "&noHomeButton=true&noLogoutButton=true&closeWindowButton=true&anonView=true"});
+						chrome.tabs.create({'windowId': results[0].windowId, 'index': results[0].index+1, 'url': "https://confs.precisionconference.com/~chi18a/adminOnePaper?userRef=" + STATE.PCS_USER_REF + "&paperNumber=" + message.paperId + "&noHomeButton=true&noLogoutButton=true&closeWindowButton=true&anonView=true"});
 					} else {
-						chrome.tabs.create({'url': "https://confs.precisionconference.com/~chi18a/adminOnePaper?userRef=" + PCS_USER_REF + "&paperNumber=" + message.paperId + "&noHomeButton=true&noLogoutButton=true&closeWindowButton=true&anonView=true"});
+						chrome.tabs.create({'url': "https://confs.precisionconference.com/~chi18a/adminOnePaper?userRef=" + STATE.PCS_USER_REF + "&paperNumber=" + message.paperId + "&noHomeButton=true&noLogoutButton=true&closeWindowButton=true&anonView=true"});
 					}
 				});
 			}
--- pcs.js
+++ pcs.js
@@ -120,9 +120,9 @@ $(function() {
 		$("#link-status").text("Installed");
 		$("#setExtensionIdButton").removeClass("invisible");
 		$("#setExtensionIdButton").click(function(event) {
-			var venueMatch = window.location.pathname.match(/venues\/(\d+)\//);
+			var venueMatch = window.location.pathname.match(/venues\/([^\/]+)\//);
 			if (venueMatch) {
-				var venueID = parseInt(venueMatch[1]);
+				var venueID = venueMatch[1];
 				chrome.runtime.sendMessage({type: "link-extension", 
 				                            hostname: window.location.host,
 				                            "venueID" : venueID}, function(data) {
@@ -148,6 +148,7 @@ $(function() {
 				$("#timerNum").change(updateFunc);
 				updateFunc();
 
+				// Only needed to support original PCS
 				$("#paper-queue").click(function(event) {
 					if ($(event.target).hasClass("paperLink")) {
 						var paperId = $(event.target).text();
